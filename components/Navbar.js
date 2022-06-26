d591b0bfabf8b47915a9757078cef2e50f196263
sunchaehoon <sunchaehoon@naver.com>
2022-06-26 17:05:12 +0900

    6/26#1.4

---
+++ components/Navbar.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+    const router = useRouter();
+
+    return (
+        <nav>
+            <Link href="/">
+                <a 
+                    className={`${styles.link} ${
+                        router.pathname === "/" ? styles.active : ""
+                    }`}>Home</a>
+            </Link>
+            <Link href="/about">
+                <a 
+                    className={`${styles.link} ${
+                        router.pathname === "/about" ? styles.active : ""
+                    }`}>About</a>
+            </Link>
+        </nav>
+    )
+}
