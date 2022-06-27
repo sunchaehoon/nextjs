c0624068a0388fecadce506cf9a376be060e5ac0
sunchaehoon <sunchaehoon@naver.com>
2022-06-27 19:47:06 +0900

    6/27nextjs#2.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
+import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 export default function App({Component, pageProps}) {
     return (
-        <>
-            <Navbar />
+        <Layout>
             <Component {...pageProps} />
-            <style jsx global>{`
-                a {
-                    color: white;
-                }
-            `}</style>
-        </>
+        </Layout>
     );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,11 @@
 import Navbar from "../components/Navbar";
+import Head from "next/head";
+import Seo from "../components/Seo";
 
 export default function About() {
     return (
         <>
+            <Seo title="About" />
             <h1>About</h1>
         </>
     );
